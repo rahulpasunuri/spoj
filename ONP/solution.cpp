61447cbd9e07684fc7a150f4c3c660f53286a15f
Rahul Pasunuri <rahul.pasunuri@gmail.com>
2014-12-12 12:27:33 -0500

    accepted solution use stacks to do it..

--- ONP/solution.cpp
+++ ONP/solution.cpp
@@ -3,8 +3,56 @@
 #include<iostream>
 using namespace std;
 
+class Stack
+{
+	private:
+		char a[400];
+		int length;
+		
+	public:
+		Stack()
+		{
+			length = 0;
+		}
+		void push(char alp)
+		{
+			a[length] = alp;
+			length++;
+		}
+		char pop()
+		{
+			length--;
+			return a[length];
+		} 
+};
+
 int main()
 {
-	//TODO
+	int numSamples = 0;
+	cin>>numSamples;
+	
+	while(numSamples>0)
+	{
+		char exp[400];
+		cin >> exp;
+		Stack st;
+		for(int i=0; exp[i]!='\0';i++)		
+		{
+			if ( exp[i] ==  '+' || exp[i] ==  '-' || exp[i] ==  '*' || exp[i] ==  '/' || exp[i] ==  '^')
+			{
+				st.push(exp[i]);
+			}
+			else if(exp[i] == ')')
+			{
+				cout<<st.pop();
+			}
+			else if(exp[i]!='(')
+			{
+				cout<<exp[i];
+			}
+		}
+		cout<<endl;
+		numSamples--;
+	}
 	return 0;
 }
